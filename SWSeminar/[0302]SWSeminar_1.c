0d5b92e4fa516722e716c9fc8a043eac4e02972f
Leeky <rbdus7174@naver.com>
2021-03-03 13:14:58 +0900

    초기 커밋.

---
+++ SWSeminar/[0302]SWSeminar_1.c
@@ -0,0 +1,8 @@
+/* [0302]SWSeminar */
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello World\n");
+
+	return 0;
+}
---
+++ SWSeminar/prog3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int length;
+	float weight;
+
+	printf("=============================\n");
+	printf("       건강 검진 결과        \n");
+	printf("=============================\n\n");
+	printf("   이름 : 강이화\n");
+	printf("   나이 : 20세\n");
+	printf("   키 : 170cm\n");
+	printf("   체중 : 50.25Kg\n");
+	printf("   혈액형 : B형\n\n");
+	printf("=============================\n");
+
+	return 0;
+
+}
