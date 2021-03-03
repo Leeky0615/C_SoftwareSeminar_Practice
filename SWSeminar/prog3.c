7cc1298d933ba81fca69c542d09a97aabe0e680b
Leeky <rbdus7174@naver.com>
2021-03-03 13:43:12 +0900

    SW기술세미나[1주차]

--- SWSeminar/prog3.c
+++ SWSeminar/prog3.c
@@ -1,19 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
-	int length;
-	float weight;
+	int height; // 키
+	float weight; // 몸무게
+	printf("키를 입력하세요 : ");
+	scanf("%d", &height);
+	printf("몸무게를 입력하세요 : ");
+	scanf("%f", &weight);
 
 	printf("=============================\n");
 	printf("       건강 검진 결과        \n");
 	printf("=============================\n\n");
 	printf("   이름 : 강이화\n");
 	printf("   나이 : 20세\n");
-	printf("   키 : 170cm\n");
-	printf("   체중 : 50.25Kg\n");
+	printf("   키 : %dcm\n",height);
+	printf("   체중 : %.2fKg\n",weight);
 	printf("   혈액형 : B형\n\n");
 	printf("=============================\n");
 
 	return 0;
-
 }
