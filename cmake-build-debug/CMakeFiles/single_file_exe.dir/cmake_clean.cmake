b6f7e1175086e7a5624e822bc25cc19cf024bda6
Leeky <rbdus7174@naver.com>
2021-05-12 14:01:48 +0900

    .

---
+++ cmake-build-debug/CMakeFiles/single_file_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../SWSeminar/single_file_exe.exe"
+  "../SWSeminar/single_file_exe.exe.manifest"
+  "../SWSeminar/single_file_exe.pdb"
+  "CMakeFiles/single_file_exe.dir/SWSeminar/[0511_Lab10]/[0511_Lab9]Exercise4.c.obj"
+  "single_file_exe.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/single_file_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
